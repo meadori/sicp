13ee90dce743943a2c20e77d2ced927cbba707f0
Meador Inge <meadori@gmail.com>
2017-05-13 21:09:00 -0500

    Solve 1.6

--- chapter-1.scm
+++ chapter-1.scm
@@ -106,3 +106,69 @@
 ;; For normal-order evaluation, `0` will be returned because
 ;; the arguments are *not* evaluated until they are used --
 ;; lazy evaluation.
+
+
+;; Exercise 1.6
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(write (sqrt 9))
+(newline)
+;; 3.00009155413138
+
+(write (sqrt (+ 100 37)))
+(newline)
+;; 11.704699917758145
+
+(write (sqrt (+ (sqrt 2) (sqrt 3))))
+(newline)
+;; 1.7739279023207892
+
+(write (square (sqrt 1000)))
+(newline)
+;; 1000.000369924366
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(write (new-if (= 2 3) 0 5))
+(newline)
+;; 5
+
+(write (new-if (= 1 1) 0 5))
+(newline)
+;; 0
+
+;; Now, consider defining `sqrt-iter` in terms of
+;; `new-if`:
+;;
+;;   (define (sqrt-iter guess x)
+;;     (new-if (good-enough? guess x)
+;;         guess
+;;         (sqrt-iter (improve guess x)
+;;                    x)))
+;;
+;; This will cause an infinite loop because `new-if` is
+;; applied as a function call, thus its arguments are
+;; evaluated before it is called.  As such, `sqrt-iter`
+;; is applied over-and-over and the base case is never
+;; triggered.
+;;
+;; This is a good example of why special forms are needed.
