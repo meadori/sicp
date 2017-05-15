3ad469f3b03c729903d7764757f1d12b6f2c2f87
Meador Inge <meadori@gmail.com>
2017-05-14 19:27:44 -0500

    Solve 1.9

--- chapter-1.scm
+++ chapter-1.scm
@@ -295,3 +295,46 @@
 ;; 3.0000000000000977
 ;; > (cube-root 10000000000000)
 ;; 21544.346900318837
+
+
+;; Exercise 1.9
+
+;; Consider:
+;;
+;;   (define (+ a b)
+;;     (if (= a 0)
+;;         b
+;;         (inc (+ (dec a) b))))
+;;
+;; Evaluate `(+ 4 5)`:
+;;
+;;   (+ 4 5)
+;;   (inc (+ 3 5))
+;;   (inc (inc (+ 2 5)))
+;;   (inc (inc (inc (+ 1 5))))
+;;   (inc (inc (inc (inc (+ 0 5)))))
+;;   (inc (inc (inc (inc 5))))
+;;   (inc (inc (inc 6)))
+;;   (inc (inc 7))
+;;   (inc 8)
+;;   9
+;;
+;; This is a linear recursive process.
+;;
+;; Consider:
+;;
+;;   (define (+ a b)
+;;     (if (= a 0)
+;;         b
+;;         (+ (dec a) (inc b))))
+;;
+;; Evaluate `(+ 4 5)`:
+;;
+;;   (+ 4 5)
+;;   (+ 3 6)
+;;   (+ 2 7)
+;;   (+ 1 8)
+;;   (+ 0 9)
+;;   9
+;;
+;; This is a linear iterative process.
