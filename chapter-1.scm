2abc15c5f501099ac5fc2a702f984ec14d5999a5
Meador Inge <meadori@gmail.com>
2017-05-29 15:55:39 -0500

    Solve 1.21

--- chapter-1.scm
+++ chapter-1.scm
@@ -753,3 +753,33 @@
 ;;
 ;; The relation to Fibonacci is consistent with Lame's Theorem given in
 ;; the text.
+
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+
+;; Exercise 1.21
+
+(write (smallest-divisor 199))
+(newline)
+;; 199
+
+(write (smallest-divisor 1999))
+(newline)
+;; 1999
+
+(write (smallest-divisor 19999))
+(newline)
+;; 7
