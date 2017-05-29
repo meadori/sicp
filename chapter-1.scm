d45587666ead913f09dbfec6a68b24b375ef82d2
Meador Inge <meadori@gmail.com>
2017-05-28 20:16:02 -0500

    Solve 1.15

--- chapter-1.scm
+++ chapter-1.scm
@@ -530,3 +530,35 @@
 ;; As the amount of change, `ac`, increases the space
 ;; complexity is O(ac).  The runtime complexity is
 ;; exponential, O(c^ac), for some constant `c`.
+
+
+;; Exercise 1.15
+
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+(write (sine 12.15))
+(newline)
+
+;; The procedure `p` is applied five times:
+;;
+;;   (sine 12.15)
+;;   = (p (sine 4.05))
+;;   = (p (p (sine 1.3499999999999999))
+;;   = (p (p (p (sine 0.44999999999999996))))
+;;   = (p (p (p (p (sine 0.15)))))
+;;   = (p (p (p (p (p (sine 0.049999999999999996))))))
+;;   = (p (p (p (p (p 0.049999999999999996)))))
+;;   = (p (p (p (p 0.1495))))
+;;   = (p (p (p 0.4351345505)))
+;;   = (p (p 0.9758465331678772))
+;;   = (p -0.7895631144708228)
+;;   = -0.39980345741334
+;;
+;; The runtime and space complexity are O(log3(a)).
